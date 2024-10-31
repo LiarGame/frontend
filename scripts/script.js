1c4c955875e0e8250dee4889b78a8bd1b9f03493
Hyeon <csh-scl>
2024-10-31 22:47:16 +0900

    feat : 웹 소켓 추가 및 스크립트 변경

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,39 @@
+import { getSocket } from '..scripts/websocket.js';
+
 // 페이지 로드 시 isHost 값 가져오기
 let isHost = localStorage.getItem('isHost') === 'true';
+
+// 웹 소켓 연결용 소켓
+let socket 
+
+window.onload = function() {
+  // url 
+  socket = new WebSocket('ws://your-websocket-server-url');
+
+  // 확인용 콘솔
+  socket.onopen = function() {
+      console.log('웹 소켓 연결이 열렸습니다.');
+  };
+
+  socket.onmessage = function(event) {
+      const response = JSON.parse(event.data);
+      if (response.type === 'ROOM_CREATED') {
+          // 동적으로 방 코드 
+          console.log('방이 생성되었습니다. 방 코드:', response.roomCode);
+          // 필요한 경우 방 코드 표시 등의 추가 작업을 수행
+      }
+  };
+
+  socket.onerror = function(error) {
+      console.error('웹 소켓 오류:', error);
+  };
+
+  socket.onclose = function() {
+      console.log('웹 소켓 연결이 닫혔습니다.');
+  };
+};
+
+
 
 function createRoom() {
   isHost = true;
