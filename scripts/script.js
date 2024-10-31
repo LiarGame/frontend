e8f8fd73dddfcd6f7ca0140ff83b6d48df882776
Hyeon <csh-scl>
2024-10-31 15:40:24 +0900

    fix : 경로 수정

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,11 @@ let isHost = localStorage.getItem('isHost') === 'true';
 function createRoom() {
   isHost = true;
   localStorage.setItem('isHost', isHost); // isHost 값을 로컬 저장소에 저장
-  location.href = "html\room-host.html"; // 방 만들기 후 페이지 이동
+  location.href = "../html/room-host.html"; // 방 만들기 후 페이지 이동
 }
 
 function joinRoom() {
   isHost = false; // 참가하기 클릭 시 isHost는 false
   localStorage.setItem('isHost', isHost); // isHost 값을 로컬 저장소에 저장
-  location.href = 'html\room-entry.html'; // 참가하기 후 페이지 이동
+  location.href = '../html/room-entry.html'; // 참가하기 후 페이지 이동
 }
