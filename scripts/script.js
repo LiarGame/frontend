e42d5a61a1c3b87213204b4a892da129fa4f8c7f
Hwang-hye-ryeong <dr4853@naver.com>
2024-11-03 03:27:24 +0900

    feat: 채팅창 추가

--- scripts/script.js
+++ scripts/script.js
@@ -144,44 +144,63 @@ window.startGame = function() {
   //3초 후에 위치 이동
   setTimeout(() => {
     pTag.style.transition = "transform 1s ease"; // 이동 시 부드러운 효과
-    pTag.style.transform = "translate(-50%, -550%)";
+    pTag.style.transform = "translate(-50%, -600%)";
   }, 3000);
-  //4.5초 후에 제시어 설명 시작
+  //4초 후에 제시어 설명 시작
   setTimeout(() => {
     explainKeyword();
   }, 4000);
 }
 
-window.input = function () {
+//입력값 유무에 따라 버튼 활성화(수정해야댐)
+window.checkInput = function () {
   const nameInput = document.getElementById('name-input');
   const roomInput = document.getElementById('room-input');
-  const text = document.querySelector('.text-wrapper-4');
+  const enterButtonHost = document.querySelector('.overlap-group-wrapper button');
+  const textHost = document.querySelector('.overlap-group-wrapper .text-wrapper-4');
   const enterButton = document.querySelector('#enterButton');
 
+  enterButtonHost.disabled = true;
   enterButton.disabled = true;
   enterButton.style.color = "gray";
   enterButton.style.cursor = "default";
 
-  // 입력 이벤트 리스너 추가
   function checkInput() {
     if(nameInput.value.length > 0) {
       nameInput.style.color = "black";
-      if (roomInput.value.length > 0) {
-        roomInput.style.color = "black";
-        enterButton.style.color = "black";
-        enterButton.style.cursor = "pointer";
-        enterButton.disabled = false;
-      }
+      enterButtonHost.disabled = false;
+      textHost.style.color = "black";
+    }
+    if (roomInput.value.length > 0) {
+      roomInput.style.color = "black";
+    }
+    if(nameInput.value.length > 0 && roomInput.value.length > 0) {
+      enterButton.style.color = "black";
+      enterButton.style.cursor = "cursor";
+      enterButton.disabled = false;
     } else {
       enterButton.style.color = "gray";
       enterButton.style.cursor = "default";
       enterButton.disabled = true;
     }
   }
-  // 각 입력 필드에 이벤트 리스너 추가
   nameInput.addEventListener('input', checkInput);
   roomInput.addEventListener('input', checkInput);
+}
 
+window.checkChatInput = function () {
+  const chatInput = document.getElementById('chatInput');
+
+  function checkChatInput() {
+    const chatSender = document.querySelector(".chat-input-area img");
+
+    if(chatInput.value.length > 0) {
+      chatSender.src = "../img/msg-send-black.png";
+    } else {
+      chatSender.src = "../img/msg-send.png";
+    }
+  }
+  chatInput.addEventListener('input', checkChatInput);
 }
 
 window.explainKeyword = function () {
@@ -191,6 +210,23 @@ window.explainKeyword = function () {
     if (chatDiv) {
       chatDiv.style.display = "block";
     }
+}
+
+window.sendMessage = function (){
+  const chatInput = document.getElementById("chatInput");
+  const chatMessages = document.getElementById("chatMessages");
+
+  if (chatInput.value.trim() !== "") {
+    // 새 메시지 추가
+    const message = document.createElement("p");
+    message.classList.add('myMsg');
+    message.innerHTML = `${chatInput.value}`;
+    // <span className="chat-sender">나</span>
+
+    chatMessages.appendChild(message); // 메시지 영역에 추가
+
+    chatInput.value = ""; // 입력 필드 초기화
+  }
 }
 
 
